6ac1b2336a4b13752f644b3125facb5df5a9f4d9
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-27 23:46:32 +0630

    [Complete] Added Feature of TransactionHistory

---
+++ Batch4.Api.MiniKpay/Features/TransactionHistory/DA_TransactionHistory.cs
@@ -0,0 +1,5 @@
+﻿namespace Batch4.Api.MiniKpay.Features.TransactionHistory;
+
+public class DA_TransactionHistory
+{
+}
---
+++ Batch4.Api.MiniKpay/Features/TransactionHistory/TransactionHistoryController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Batch4.Api.MiniKpay.Features.TransactionHistory;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TransactionHistoryController : ControllerBase
+{
+}
