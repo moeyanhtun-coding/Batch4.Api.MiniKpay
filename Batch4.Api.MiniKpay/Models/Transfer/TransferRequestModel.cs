1be174a8c126c50efd6dba570d3660480bbb58e9
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-28 15:10:14 +0630

    -

---
+++ Batch4.Api.MiniKpay/Models/Transfer/TransferRequestModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Batch4.Api.MiniKpay.Models.Transfar;
+
+public class TransferRequestModel
+{
+    public string FromMobileNo { get; set; }
+    public string ToMobileNo { get; set; }
+    public decimal Amount { get; set; }
+
+    public void IsValid()
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(FromMobileNo))
+                throw new Exception("Invalid From Mobile No.");
+            if (string.IsNullOrEmpty(ToMobileNo))
+                throw new Exception("Invalid To Mobile No.");
+            if (Amount <= 0)
+                throw new Exception("Invalid Amount");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.ToString());
+        }
+    }
+}
