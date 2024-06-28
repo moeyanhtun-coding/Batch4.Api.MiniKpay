1df6ddaf80ea754cb35a09d7d965de325e8d4be0
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-28 15:10:33 +0630

    [Add] Mobile No For Customer Balance Model

--- Batch4.Api.MiniKpay/Models/CustomerModel.cs
+++ Batch4.Api.MiniKpay/Models/CustomerModel.cs
@@ -13,4 +13,5 @@ public class CustomerBalanceModel
     public int CustomerBalanceId { get; set; }
     public string CustomerCode { get; set; }
     public decimal Balance { get; set; }
+    public string MobileNo { get; set; }
 }
