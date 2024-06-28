856757ecd0f64df6daa55b7d2fa412c2bfb4d3fe
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-28 16:47:35 +0630

    [Complete] DA_TransactionHistory

--- Batch4.Api.MiniKpay/Features/TransactionHistory/DA_TransactionHistory.cs
+++ Batch4.Api.MiniKpay/Features/TransactionHistory/DA_TransactionHistory.cs
@@ -1,5 +1,31 @@
-﻿namespace Batch4.Api.MiniKpay.Features.TransactionHistory;
+﻿using Batch4.Api.MiniKpay.Models;
+using Dapper;
+using System.Data;
+
+namespace Batch4.Api.MiniKpay.Features.TransactionHistory;
 
 public class DA_TransactionHistory
 {
+    private readonly IDbConnection _db;
+
+    public DA_TransactionHistory(IDbConnection db)
+    {
+        _db = db;
+    }
+
+    public async Task<bool> IsExistCustomerCode(string customerCode)
+    {
+        string query = @"select * from Tbl_Customer with (nolock) where CustomerCode = @CustomerCode";
+        var item = await _db.QueryFirstOrDefaultAsync<CustomerModel>(query, new { CustomerCode = customerCode });
+        return item != null;
+    }
+
+    public async Task<List<CustomerTransactionHistoryModel>> TransactionHistoryByCustomerCode(string customerCode)
+    {
+        string query = @"select CTH.* from [dbo].[Tbl_CustomerTransactionHistory] CTH
+                                       inner join Tbl_Customer C on CTH.FromMobileNo = C.MobileNo
+                                                    where CustomerCode = @CustomerCode";
+        var lst = await _db.QueryAsync<CustomerTransactionHistoryModel>(query, new { CustomerCode = customerCode });
+        return lst.ToList();
+    }
 }
