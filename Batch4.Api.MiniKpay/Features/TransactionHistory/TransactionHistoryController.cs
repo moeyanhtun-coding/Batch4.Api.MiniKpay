0254cb18868378b661022379298e7e6048b005b9
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-28 16:48:19 +0630

    [Complete] TransactionHistoryController

--- Batch4.Api.MiniKpay/Features/TransactionHistory/TransactionHistoryController.cs
+++ Batch4.Api.MiniKpay/Features/TransactionHistory/TransactionHistoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Batch4.Api.MiniKpay.Models.TransactionHistory;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Batch4.Api.MiniKpay.Features.TransactionHistory;
@@ -7,4 +8,29 @@ namespace Batch4.Api.MiniKpay.Features.TransactionHistory;
 [ApiController]
 public class TransactionHistoryController : ControllerBase
 {
+    private readonly BL_TransactionHistory _bL_TransactionHistory;
+
+    public TransactionHistoryController(BL_TransactionHistory bL_TransactionHistory)
+    {
+        _bL_TransactionHistory = bL_TransactionHistory;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> TransactionHistory(TransactionHistoryRequestModel requestModel)
+    {
+        try
+        {
+            var result = requestModel.IsValid();
+            if (!result) 
+            {
+                return BadRequest("Invalid CustomerCode");
+            }
+            var model = await _bL_TransactionHistory.TransactionHistory(requestModel);
+            return Ok(model);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.ToString());
+        }
+    }
 }
