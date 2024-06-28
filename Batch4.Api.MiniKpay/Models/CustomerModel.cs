47206bcaa0192f5f235efbe7278587c0da347cdf
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-28 12:33:58 +0630

    [Add] CustomerModel

---
+++ Batch4.Api.MiniKpay/Models/CustomerModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Batch4.Api.MiniKpay.Models;
+
+public class CustomerModel
+{
+    public int CustomerId { get; set; }
+    public string CustomerCode { get; set; }
+    public string CustomerName { get; set; }
+    public string MobileNo { get; set; }
+}
+
+public class CustomerBalanceModel
+{
+    public int CustomerBalanceId { get; set; }
+    public string CustomerCode { get; set; }
+    public decimal Balance { get; set; }
+}
