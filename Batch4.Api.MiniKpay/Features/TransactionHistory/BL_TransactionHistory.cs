d40d6f009dc73b16772c6e84638a7e7602c39384
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-28 16:47:54 +0630

    [Complete] BL_TransactionHistory

--- Batch4.Api.MiniKpay/Features/TransactionHistory/BL_TransactionHistory.cs
+++ Batch4.Api.MiniKpay/Features/TransactionHistory/BL_TransactionHistory.cs
@@ -1,5 +1,29 @@
-﻿namespace Batch4.Api.MiniKpay.Features.TransactionHistory;
+﻿using Batch4.Api.MiniKpay.Models.TransactionHistory;
+
+namespace Batch4.Api.MiniKpay.Features.TransactionHistory;
 
 public class BL_TransactionHistory
 {
+    private readonly DA_TransactionHistory _dA_TransactionHistory;
+
+    public BL_TransactionHistory(DA_TransactionHistory dA_TransactionHistory)
+    {
+        _dA_TransactionHistory = dA_TransactionHistory;
+    }
+
+    public async Task<TransactionHistoryResponseModel> TransactionHistory(
+        TransactionHistoryRequestModel requestModel
+    )
+    {
+        bool IsExit = await _dA_TransactionHistory.IsExistCustomerCode(requestModel.CustomerCode!);
+        if (!IsExit)
+            throw new Exception("Customer doesn't Exist");
+
+        var lst = await _dA_TransactionHistory.TransactionHistoryByCustomerCode(requestModel.CustomerCode!);
+        var model = new TransactionHistoryResponseModel()
+        {
+            Data = lst
+        };
+        return model;
+    }
 }
