dce3949d448ea2f0a7c330071bd4d3c0ed0576dd
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-28 12:34:44 +0630

    [Added] Transactoin History Model

---
+++ Batch4.Api.MiniKpay/Models/TransactionHistory/TransactionHsitoryRequestModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Batch4.Api.MiniKpay.Models.TransactionHistory;
+
+public class TransactionHsitoryRequestModel
+{
+    public string? CustomerCode { get; set; }
+
+    public void IsValid()
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(CustomerCode))
+                throw new Exception("Invalid CustomerCode");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.ToString());
+        }
+    }
+}
---
+++ Batch4.Api.MiniKpay/Models/TransactionHistory/TransactionHsitoryResponseModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Batch4.Api.MiniKpay.Models.TransactionHistory;
+
+public class TransactionHsitoryResponseModel
+{
+    public List<CustomerTransactionHistoryModel> Data { get; set; }
+}
